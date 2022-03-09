80588e536fc9d4ce6e3cbe4cf88b66186f5b1197
tekpill <ankitwww@gmail.com>
2022-03-09 00:04:11 -0800

    Graph - Has cycle

---
+++ structy/067_graph_has_cycle.js
@@ -0,0 +1,35 @@
+//Write a function, hasCycle, that takes in an object representing the
+//adjacency list of a directed graph.
+//The function should return a boolean indicating whether or not the graph contains a cycle.
+
+const hasCycle = (graph) => {
+  const visited = new Set();
+  for (let startNode in graph) {
+    if (cycleDetect(graph, startNode, new Set(), visited)) return true;
+  }
+  return false;
+};
+
+const cycleDetect = (graph, node, visiting, visited) => {
+  if (visited.has(node)) return false;
+
+  if (visiting.has(node)) return true;
+
+  visiting.add(node);
+
+  for (let neighbor of graph[node]) {
+    if (cycleDetect(graph, neighbor, visiting, visited)) return true;
+  }
+
+  visiting.delete(node);
+  visited.add(node);
+  return false;
+};
+
+console.log(
+  hasCycle({
+    a: ["b"],
+    b: ["c"],
+    c: ["a"],
+  })
+); // true
